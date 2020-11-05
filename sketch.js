2c7e268c5f16d2e2d9063d40ecd397b20b06150f
anshika4 <72254585+anshika4@users.noreply.github.com>
2020-11-05 13:03:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,22 +11,20 @@ function preload()
 }
 
 function setup() {
-	createCanvas(600, 500);
+	createCanvas(500, 400);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	ground=new Ground(240,500,900,20);
-	stand1=new Dvision(10,450,10,200);
-	stand2=new Dvision(100,450,10,200);
-	stand3=new Dvision(200,450,10,200);
-	stand4=new Dvision(300,450,10,200);
-	stand5=new Dvision(400,450,10,200);
-	stand6=new Dvision(500,450,10,200);
-	stand7=new Dvision(600,450,10,200);
-	stand8=new Dvision(700,450,10,200);
-	stand9=new Dvision(800,450,10,200);
+	ground=new Ground(240,400,900,20);
+	stand1=new Dvision(10,350,10,200);
+	stand2=new Dvision(100,350,10,200);
+	stand3=new Dvision(200,350,10,200);
+	stand4=new Dvision(300,350,10,200);
+	stand5=new Dvision(400,350,10,200);
+	stand6=new Dvision(500,350,10,200);
+	
 	
 	for (var i =35; i <=width; i=i+50) {
     plinkos.push(new Plinko(i,75,10));
@@ -43,23 +41,6 @@ function setup() {
 	 for (var i = 10; i <=width-10; i=i+50) {
      plinkos.push(new Plinko(i,180,10));
 	 }
-	 
-	 for (var i =35; i <=width; i=i+50) {
-    plinkos.push(new Plinko(i,215,10));
-	}
-  
-	for (var i = 10; i <=width-10; i=i+50) {
-	plinkos.push(new Plinko(i,250,10));
-	}
-  
-	for (var i = 35; i <=width; i=i+50) {
-    plinkos.push(new Plinko(i,285,10));
-	}
-  
-	 for (var i = 10; i <=width-10; i=i+50) {
-     plinkos.push(new Plinko(i,320,10));
-	 }
-  
   
 
 	Engine.run(engine);
@@ -84,12 +65,8 @@ function draw() {
   stand5.display();
   fill("orange");
   stand6.display();
-  fill("yellow");
-  stand7.display();
-  fill("red");
-  stand8.display();
-  fill("red");
-  stand9.display();
+  
+  
  
  
   for(var i=0;i<plinkos.length;i++){
