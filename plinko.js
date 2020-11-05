1447acda3d1f782588baa3f2633f3811abbaa010
anshika4 <72254585+anshika4@users.noreply.github.com>
2020-11-05 13:04:23 +0530

    Update plinko.js

--- plinko.js
+++ plinko.js
@@ -3,15 +3,15 @@ class Plinko {
     var options = {
         isStatic:true,
     }
-    this.body = Bodies.circle(x, y,10,options);
-  this.radius=10;
+    this.body = Bodies.circle(x, y,8,options);
+  this.radius=8;
     World.add(world, this.body);
   }
   display(){
     var pos =this.body.position;
     push();
     translate(pos.x, pos.y);
-  ellipseMode(CENTER);
+  ellipseMode(RADIUS);
     fill("orange");
     ellipse(0, 0,this.radius,this.radius);
     pop();
